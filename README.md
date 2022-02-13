803d799eade013a1e7d6b3adc49973b836506667
NN112-TH <72507375+NN112-TH@users.noreply.github.com>
2022-02-13 08:27:02 +0000

    clean app.py make small docs

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# backend
+This is the api section github repository for UCL resolve a project for a UCL Hackathon 2022. 
+The api hosted at [heroku](http://127.0.0.1:5000/report)
+The frontend section is located at [Here](https://github.com/ucl-hackathon-2022/frontend)
+
+These are the procedure for installing/running project
+
 ### setting the environment
 $ python -m venv venv
 // mac
@@ -5,8 +12,10 @@ $ source venv/bin/activate
 // windows
 $ venv\Scripts\activate
 
-then install flask 
+then install flask and other dependency by typing
 
 $ pip install -r requirements.txt
 
-flask run
+### running the application
+we are going to developing the application by
+$ flask run
--- app.py
+++ app.py
@@ -13,93 +13,17 @@ cors = CORS(app)
 
 
 # Global variables
-API_TOKEN = 'uclapi-c534250c5533a5e-969273a8d8b716f-172fc10b1563d07-72b9a381d300f82'
-CLIENT_ID = '7083760871512299.0586240432827629'
-CLIENT_SECRET = '5110d3ae18ef449a6a4f2a9a489f071ac5c03e8eb3acb05d9a7081067589f36d'
 RECEIVER_EMAIL = 'example.api.ucl.sender@gmail.com'
 PASSWORD = 'Example1234'
 
 app.config['DEBUG'] = True
 
-# # Temp
-# locations_id_map = {'id_1': {'facility_type': 'bathroom',
-#                              'location': 'Wilkins'},
-#                     'id_2': {'facility_type': 'classroom',
-#                              'location': 'Main Quad Pop-up Hub Room 101'}}
-
-
+# for testing in the remote sever
 @app.route('/', methods=['GET'])
 def hello():
     return 'hello world', 200
 
-# Posting data to the client
-@app.route('/callbackParam', methods=['POST'])
-def addData():
-    '''Data that we need from the user.'''
-    username = request.json['username']
-    password = request.json['password']
-    print(request)
-    res = {
-        'username': username,
-        'password': password
-    }
-    return res, 200
-
-
-url = f"https://uclapi.com/oauth/authorise/?client_id={CLIENT_ID}&state=1"
-
-# Posting data to the client
-@app.route('/login')
-def uclapi_login():
-    return redirect(url)
-
-# # Posting data to the client
-# @app.route('/complete')
-# def uclapi_complete():
-#     return redirect(url)
-
-@app.route('/callback')
-def receive_callback():
-    # receive parameters 
-    # result = request.args.get('result')
-    code = request.json['code']
-    # state = request.args.get('state')
-    params = {
-        "client_secret": CLIENT_SECRET,
-        "client_id ": CLIENT_ID,
-        "code": code,
-    }
-    r = requests.get('https://uclapi.com/oauth/token', params=params)
-    print(r.json())
-    # e.g. request an auth token from /oauth/token
-    return 'done'
-
-# @app.route('/oauth', methods=['GET'])
-# def receive_callback():
-#     # receive parameters
-#     result = request.json['result']
-#     code = request.json['code']
-#     state = request.json['state']
-
-#     print(request)
-#     print(result)
-#     print('code', code)
-#     if result == 1:
-#         return state, 200
-#     print(request)
-#     # do something with these parameters
-#     # e.g. request an auth token from /oauth/token
-#     '''
-#     params = {
-#         "client_id": "123.456",
-#         "code": "1",
-#         "client_secret": "secret",
-#     }
-#     r = request.get("https://uclapi.com/oauth/token", params=params)
-#     print(r.json())
-#     '''
-#     return code, 200
-
+# for integrating with the frontend solution
 @app.route('/report', methods=['POST'])
 def report():
     id = request.json['id']
@@ -120,14 +44,6 @@ def report():
     email.send("Report Issue", message.getMessage())
 
     return 'done', 200
-
-# @app.route('/sendEmail', methods=['GET'])
-# def email():
-#     email = emailService(RECEIVER_EMAIL, RECEIVER_EMAIL, PASSWORD)
-#     message = emailMessageFormat("john@gmail.com", datetime.now().strftime("%d/%m/%Y, %H:%M:%S"), "Location", "event", "bathroom")
-#     print(message.getMessage())
-#     email.send("Report Issue", message.getMessage())
-#     return "done", 200
 
 if __name__ == '__main__':
     app.run()
